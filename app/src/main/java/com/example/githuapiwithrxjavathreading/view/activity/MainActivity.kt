18e19a8baeeed8e0060ca916392a95cd639618e8
Soulem <vivikabado@gmail.com>
2021-07-19 12:08:47 -0400

    Updated Main Activity
    Updated Main activity to fill SearchLayoutFragment

--- app/src/main/java/com/example/githuapiwithrxjavathreading/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/githuapiwithrxjavathreading/view/activity/MainActivity.kt
@@ -12,8 +12,6 @@ import com.example.githuapiwithrxjavathreading.utl.GitAPISelector
 import com.example.githuapiwithrxjavathreading.view.fragment.DisplayItemFragment
 import com.example.githuapiwithrxjavathreading.view.fragment.RecyclerDisplayFragment
 import com.example.githuapiwithrxjavathreading.view.fragment.SearchLayoutFragment
-import com.example.githuapiwithrxjavathreading.viewmodel.ObjectViewModel
-import kotlinx.android.parcel.RawValue
 
 class MainActivity : AppCompatActivity(), GitAPISelector {
     private lateinit var binding: ActivityMainBinding
@@ -25,15 +23,8 @@ class MainActivity : AppCompatActivity(), GitAPISelector {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //hFA = HomeFragmentAdapter(supportFragmetnManager)
-        // binding.mainVp.adapter = hFA
 
         MultiDex.install(this)
-        //searchLayoutFragment = supportFragmentManager.findFragmentById(R.id.search_frameLayout) as SearchLayoutFragment
-        //searchLayoutFragment.setAdapter(recyclerDisplayFragment.getAdapter())
-        //var user = ObjectViewModel.instance.gitRepoData.value?.get(0) as GitRetrofitUser
-        //if (user == null){
-        //}
         val searchFragment = SearchLayoutFragment.getInstance()
         supportFragmentManager.beginTransaction()
             .add(R.id.detail_item_frame, searchFragment)
